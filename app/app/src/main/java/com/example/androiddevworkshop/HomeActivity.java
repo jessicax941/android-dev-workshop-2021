a49a2470f31e1e6366691f67983975b871749505
Jessica Phua Shu Xin <jessicax941@gmail.com>
2021-02-05 12:00:05 +0800

    Exercise 3.2: Adding a ListView

--- app/app/src/main/java/com/example/androiddevworkshop/HomeActivity.java
+++ app/app/src/main/java/com/example/androiddevworkshop/HomeActivity.java
@@ -3,12 +3,37 @@ package com.example.androiddevworkshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ListView foodListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        foodListView = findViewById(R.id.list_view);
+
+        ArrayList<String> foods = new ArrayList<>();
+        // Add dummy data
+        String[] stringArray = {"Cheese Burger", "Truffle Fries", "Vegan Burger", "Vanilla Ice Cream",
+                "Chocolate Sundae"};
+        Collections.addAll(foods, stringArray);
+        System.out.println("foodsList: " + foods.toString());
+
+        // Adapter for filling ListView with data
+        ArrayAdapter<String> foodsAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1, // Built-in layout for a list item
+                foods
+        );
+
+        foodListView.setAdapter(foodsAdapter);
     }
 }
